e52d9e53ff7db84787ef034e2728b1798e523998
kmgyu <lidersy961@gmail.com>
2024-12-12 21:36:49 +0900

    todo-test ready to func

--- back-end_test/todo_test.py
+++ back-end_test/todo_test.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 import dotenv
 import os
 import json
@@ -22,20 +23,43 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 # 노지 db 연동
 class FieldTodo(db.Model):
     __tablename__ = 'field_info'
-    field_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    field_name = db.Column(db.String(255), nullable=False)
-    lat_arr = db.Column(db.Text, nullable=False)
-    lng_arr = db.Column(db.Text, nullable=False)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task_name = db.Column(db.String(255), nullable=False)
+    task_content = db.Column(db.Text, nullable=False)
     # 아래는 수정 더미데이터들
-    field_address = db.Column(db.String(255), default='mokpo, 임시 값이다.')
-    crop_name = db.Column(db.String(255), default='상추, 임시값이다.')
-    field_area = db.Column(db.Float, default=0.0)
-    farm_startdate = db.Column(db.Date, default=date.today)
+    cycle = db.Column(db.Integer, default=1)
+    start_date = db.Column(db.TIMESTAMP, default=func.now())  # 기본값: 현재 시간
+    period = db.Column(db.Integer, default='상추, 임시값이다.')
 
+@app.route('/')
+def index():
+    return render_template('main.html')
 
+@app.route('/add-field-todo', methods=['GET', 'POST'])
+def add_field_todo():
+    # 요청 데이터
+    data = request.get_json()
+
+    field_data = data['fieldData'] # 위도 경도 저장
+    field_id = field_data['name']  # 필드 id, 노지 입력란으로 일단 해둠
+    # 위도, 경도 배열 추출
+    lat_arr = json.dumps([point['lat'] for point in field_data['polygon']])
+    lng_arr = json.dumps([point['lng'] for point in field_data['polygon']])
+
+    # 새 필드 생성 및 DB 저장
+    field = FieldTodo(
+        field_name=field_id,
+        lat_arr=lat_arr,
+        lng_arr=lng_arr,
+    )
+    db.session.add(field)
+    db.session.commit()
+
+    return jsonify({"message": "Field saved successfully!"}), 200
 
 if __name__ == '__main__':
     # http://orion.mokpo.ac.kr:8483/
