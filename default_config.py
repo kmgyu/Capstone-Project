005304317ff53d2d12bbf7607828f7165eac1fcf
kmgyu <lidersy961@gmail.com>
2024-12-24 15:41:51 +0000

    [remove] main code removal for test

--- default_config.py
+++
@@ -1,9 +0,0 @@
-import dotenv
-import os
-
-dotenv.load_dotenv()
-
-API_KEY=os.environ.get('API_KEY')
-SECRET_KEY = 'your_secret_key'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
-DEBUG = True
